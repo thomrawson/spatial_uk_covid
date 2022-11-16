223252f5f92f7f49fc6700b3edba3b7aaa20f7b7
Thomas Rawson <trawson@dide.local>
2022-11-16 23:07:38 +0000

    Installation instructions

---
+++ install_packages.R
@@ -0,0 +1,19 @@
+#This quick bit of code will ensure you have all the packages needed:
+
+rm(list = ls())
+#Set up the needed packages:
+packages <- c("tidyverse", "INLA", "rstan", "sf", "spdep")
+miss_pkgs <- packages[!packages %in% installed.packages()[,1]]
+if(length(miss_pkgs) > 0){
+  install.packages(miss_pkgs)
+}
+
+#If INLA package doesn't install you may need to run this:
+install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+
+
+#This will load the packages if needed
+#invisible(lapply(packages, library, character.only = TRUE))
+
+
+rm(miss_pkgs, packages)
