1b562a27b9c18fe7fb0907e59e79e3ce7f1fc8e9
Thom Rawson <t.rawson@imperial.ac.uk>
2022-11-29 11:06:56 +0000

    rstan install instructions

--- install_packages.R
+++ install_packages.R
@@ -2,7 +2,7 @@
 
 rm(list = ls())
 #Set up the needed packages:
-packages <- c("tidyverse", "INLA", "rstan", "sf", "spdep", "orderly",
+packages <- c("tidyverse", "INLA", "sf", "spdep", "orderly",
               "pkgdepends")
 miss_pkgs <- packages[!packages %in% installed.packages()[,1]]
 if(length(miss_pkgs) > 0){
@@ -24,4 +24,12 @@ rm(miss_pkgs, packages)
 #respective operating system for more instructions:
 #https://github.com/stan-dev/rstan/wiki/Configuring-C---Toolchain-for-Windows
 
+#First you need Tools42
+#Download and install from here:
+#https://cran.r-project.org/bin/windows/Rtools/rtools42/rtools.html
+
+#he version of RStan currently available on CRAN is not compatible with R4.2
+#Instead, you will need to install the preview of rstan 2.26 using:
+install.packages("StanHeaders", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 
