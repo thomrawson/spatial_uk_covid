112c803618c0764de3dd8034f4a04c82224dc31e
Thom Rawson <t.rawson@imperial.ac.uk>
2023-08-24 17:32:56 +0100

    Set up new task just for an acquired immunity estimate

---
+++ src/13_acquired_immunity_estimate/script.R
@@ -0,0 +1,9 @@
+#orderly::orderly_develop_start("11_figure_demos", use_draft = "newer", parameters = list(tree_depth = 15, total_iterations = 4000, n_chains = 16))
+
+#This script loads in the Case Rates data object and tries to build an estimate for acquired immunity
+#For use in the counterfactuals
+
+
+
+
+
