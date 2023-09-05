e3f0c4fd6ff36bbf1df4e42f80e9e8e5267c54b3
Thom Rawson <t.rawson@imperial.ac.uk>
2023-09-05 20:51:30 +0100

    Fix missing bracket

--- src/09k_model_fit_no_zeta_no_mobility/script.R
+++ src/09k_model_fit_no_zeta_no_mobility/script.R
@@ -297,7 +297,7 @@ real log_lik[N, T]; // Log-likelihood for each data point
 
   for (n in 1:N) {
     for (t in 1:T) {
-      log_lik[n, t] = poisson_log_lpmf(y[n, t] | log((susc_scaling*susceptible_proxy[n,t]) * (E[n,t]) + dot_product(x[t,n,], betas) + beta_random_walk[t] + theta[n]);
+      log_lik[n, t] = poisson_log_lpmf(y[n, t] | log((susc_scaling*susceptible_proxy[n,t]) * (E[n,t])) + dot_product(x[t,n,], betas) + beta_random_walk[t] + theta[n]);
     }                                               
   }
 }
@@ -364,7 +364,7 @@ real log_lik[N, T]; // Log-likelihood for each data point
 
   for (n in 1:N) {
     for (t in 1:T) {
-      log_lik[n, t] = poisson_log_lpmf(y[n, t] | log((susceptible_proxy[n,t]) * (E[n,t]) + dot_product(x[t,n,], betas) + beta_random_walk[t] + theta[n]);
+      log_lik[n, t] = poisson_log_lpmf(y[n, t] | log((susceptible_proxy[n,t]) * (E[n,t])) + dot_product(x[t,n,], betas) + beta_random_walk[t] + theta[n]);
     }                                               
   }
 }
